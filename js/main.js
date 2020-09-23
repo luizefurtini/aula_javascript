f284dff29b9f2f98ce8bb9878a534c09a5677628
luiz eduardo furtini <luizefurtini@gmail.com>
2020-09-23 19:21:17 -0300

    Aula de javascript

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por cliclar</b>";
+
+}
+
+function redirecionar(){
+    window.open("http://globallabs.academy");
+}
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "obrigado por passar o mouse";
+    alert("trocar texto");
+}
+
+function voltar(){
+    document.getElementById("mousemove").innerHTML = "passou o mouse aqui";
+
+}
